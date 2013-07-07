a594ca623318204471954f7095368008dd873ddf
semerj <jsemer@gmail.com>
2013-07-07 10:36:49 -0700

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -2,23 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import csv
-from IPython import embed
+#from IPython import embed
 
 driver = webdriver.Firefox()
 
 def get_data(url):
-  driver.get(url)
+	driver.get(url)
 	time.sleep(5)
 	elements = driver.find_elements_by_css_selector('.hospitals_score')
+	grade = driver.find_element_by_css_selector('.d-col1').get_attribute('data-grade')
+	lat = driver.find_element_by_css_selector('.d-col1').get_attribute('data-lat')
+	lon = driver.find_element_by_css_selector('.d-col1').get_attribute('data-lon')
+	address = driver.find_element_by_css_selector('.d-add').text
 	txt_list = []
+	txt_list.extend([grade, address, lat, lon])
 	for element in elements:
 	    txt_list.append(element.text)
-	#txt_list = [txt_list]
-	#print txt_list
 	return txt_list
-
-f = open('state_abrv.txt')
-state_list = f.read().split() 
 
 def open_state_file(file):
 	all_data = []
@@ -26,16 +26,16 @@ def open_state_file(file):
 	url_list = csv.reader(s)
 	for line in url_list:
 		data_result = get_data(line[1])
-		#line.append(data_result)
 		line = line + data_result
-		#embed()
 		all_data.append(line)
-		#embed()
 	return all_data
 
-#for s in state_list:
-with open("AK_data.csv", "wb") as f:
-	writer = csv.writer(f, delimiter = ",")
-	writer.writerows(open_state_file('AK'))
+f = open('state_abrv.txt')
+state_list = f.read().split() 
+
+for s in state_list:
+	with open(s + "_data.csv", "wb") as f:
+		writer = csv.writer(f, delimiter = ",")
+		writer.writerows(open_state_file(s))
 
 driver.close()
