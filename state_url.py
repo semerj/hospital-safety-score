8e794c4b47f58d59348314cdb9475848ce3a0cf1
semerj <jsemer@gmail.com>
2013-10-29 16:07:42 -0700

    Update state_url.py

--- state_url.py
+++ state_url.py
@@ -18,8 +18,8 @@ def search(state):
 		wtr.writerows(hosp_list)
 
 states = ["AL","AK","AZ","AR","CA","CO","CT","DC","DE","FL","GA","HI","ID","IL","IN","IA","KS",
-		  "KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC",
-		  "ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+	  "KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC",
+	  "ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 
 driver = webdriver.Firefox()
 driver.get("http://www.hospitalsafetyscore.org/")
