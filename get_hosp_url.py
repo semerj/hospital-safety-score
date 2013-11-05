bb3191962a44f1efd84db7ac2ed63bc58282ef20
semerj <jsemer@gmail.com>
2013-11-05 14:25:20 -0800

    Update get_hosp_url.py

--- get_hosp_url.py
+++ get_hosp_url.py
@@ -17,9 +17,11 @@ def search(state):
 		wtr = csv.writer(f, delimiter = ',')
 		wtr.writerows(hosp_list)
 
-states = [	"AL","AK","AZ","AR","CA","CO","CT","DC","DE","FL","GA","HI","ID","IL","IN","IA","KS",
-			"KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC",
-			"ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+states = [
+	"AL","AK","AZ","AR","CA","CO","CT","DC","DE","FL","GA","HI","ID","IL","IN","IA","KS",
+	"KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC",
+	"ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"
+	]
 
 driver = webdriver.Firefox()
 driver.get("http://www.hospitalsafetyscore.org/")
