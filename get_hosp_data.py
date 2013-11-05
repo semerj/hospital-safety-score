b69c7ef730daf0a62fe087e48849b6dd5efb6aee
semerj <jsemer@gmail.com>
2013-11-05 14:26:13 -0800

    Update get_hosp_data.py

--- get_hosp_data.py
+++ get_hosp_data.py
@@ -32,9 +32,11 @@ def open_state_file(file):
 		all_data.append(line)
 	return all_data
 
-states = [	"AL","AK","AZ","AR","CA","CO","CT","DC","DE","FL","GA","HI","ID","IL","IN","IA","KS",
-			"KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC",
-			"ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+states = [
+	"AL","AK","AZ","AR","CA","CO","CT","DC","DE","FL","GA","HI","ID","IL","IN","IA","KS",
+	"KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC",
+	"ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"
+	]
 
 for state in states:
 	with open('hosp_data/' + state + "_data.csv", "wb") as f:
